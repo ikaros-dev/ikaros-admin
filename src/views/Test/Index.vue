ac2d3c399cb561b9a9799e8fcefe7525ea3e9b90
li-guohao <git@liguohao.cn>
2022-10-01 10:28:36 +0800

    add FilePondUpload.vue show

--- src/views/Test/Index.vue
+++ src/views/Test/Index.vue
@@ -1,22 +1,36 @@
 <template>
   <page-header-wrapper :title="false">
     <H2>Test</H2>
+    <br>
+
     <div>
+      <h2>FileUpload.vue</h2>
+      <button @click="upload.visible = true">展示</button>
       <FileUpload :visible.sync="upload.visible" />
+    </div>
+    <br>
+
+    <div>
+      <h2>FilePondUpload.vue</h2>
+      <FilePondUpload :ref="upload" :uploadHandler="uploadHandler"></FilePondUpload>
     </div>
   </page-header-wrapper>
 </template>
 
 <script>
 import FileUpload from '@/components/File/FileUpload.vue'
+import FilePondUpload from '@/components/Upload/FilePondUpload.vue'
 
 export default {
   name: 'Test',
-  components: { FileUpload },
+  components: { FileUpload, FilePondUpload },
   data () {
     return {
       upload: {
-        visible: true
+        visible: false
+      },
+      uploadHandler: (file, options) => {
+        // todo call api to upload file
       }
     }
   },
