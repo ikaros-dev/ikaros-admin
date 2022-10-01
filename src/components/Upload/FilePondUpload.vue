3ef1ba4b306de8fe474323e299eb27977e5bf498
li-guohao <git@liguohao.cn>
2022-10-01 10:21:50 +0800

    add file upload component

---
+++ src/components/Upload/FilePondUpload.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <file-pond
+      ref="pond"
+      :accepted-file-types="accepts"
+      :allow-multiple="multiple"
+      :allowImagePreview="true"
+      :allowRevert="false"
+      :files="fileList"
+      :label-idle="label"
+      :maxFiles="20"
+      :maxParallelUploads="5"
+      :name="name"
+      :server="server"
+      fileValidateTypeLabelExpectedTypes="请选择 {lastType} 格式的文件"
+      labelFileProcessing="上传中"
+      labelFileProcessingAborted="取消上传"
+      labelFileProcessingComplete="上传完成"
+      labelFileProcessingError="上传错误"
+      labelFileTypeNotAllowed="不支持当前文件格式"
+      labelTapToCancel="点击取消"
+      labelTapToRetry="点击重试"
+      @init="handleFilePondInit">
+    </file-pond>
+  </div>
+</template>
+<script>
+import vueFilePond from 'vue-filepond'
+import 'filepond/dist/filepond.min.css'
+
+// Plugins
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css'
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+
+// Create component and register plugins
+const FilePond = vueFilePond(
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateType)
+export default {
+  name: 'FilePondUpload',
+  components: {
+    FilePond
+  },
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: 'file'
+    },
+    field: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    multiple: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    accepts: {
+      type: Array,
+      required: false,
+      default: () => {
+        return null
+      }
+    },
+    label: {
+      type: String,
+      required: false,
+      default: '点击选择文件或将文件拖拽到此处'
+    },
+    uploadHandler: {
+      type: Function,
+      required: true
+    }
+  },
+  data: function () {
+    return {
+      server: {
+        process: (fieldName, file, metadata, load, error, progress, abort) => {
+          // todo get token
+          const CancelToken = null
+          const source = CancelToken.source()
+          this.uploadHandler(
+            file,
+            {
+              onUploadProgress: progressEvent => {
+                if (progressEvent.total > 0) {
+                  progress(progressEvent.lengthComputable, progressEvent.loaded, progressEvent.total)
+                }
+              },
+              cancelToken: source.token
+            },
+            this.field
+          )
+            .then(response => {
+              load(response)
+              this.$notification.success('Uploaded successfully', response)
+              this.$emit('success', response, file)
+            })
+            .catch(failure => {
+              this.$notification.failure('Failed to upload file', failure)
+              this.$emit('failure', failure, file)
+              error()
+            })
+          return {
+            abort: () => {
+              abort()
+              this.$notification.success('Upload operation aborted by the user')
+              source.cancel('Upload operation canceled by the user.')
+            }
+          }
+        }
+      },
+      fileList: []
+    }
+  },
+  methods: {
+    handleFilePondInit () {
+      console.log('FilePond has initialized')
+    },
+    handleClearFileList () {
+      this.$refs.pond.removeFiles()
+    }
+  }
+}
+</script>
--- src/views/Test/Index.vue
+++ src/views/Test/Index.vue
@@ -1,11 +1,27 @@
 <template>
   <page-header-wrapper :title="false">
     <H2>Test</H2>
+    <div>
+      <FileUpload :visible.sync="upload.visible" />
+    </div>
   </page-header-wrapper>
 </template>
 
 <script>
+import FileUpload from '@/components/File/FileUpload.vue'
+
 export default {
-  name: 'Test'
+  name: 'Test',
+  components: { FileUpload },
+  data () {
+    return {
+      upload: {
+        visible: true
+      }
+    }
+  },
+  method: {
+
+  }
 }
 </script>
