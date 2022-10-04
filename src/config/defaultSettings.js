4aa8d7ee5dce23eb6d5ca37c898c21bb83900482
li-guohao <git@liguohao.cn>
2022-10-04 12:27:03 +0800

    rename user layout title

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -23,7 +23,7 @@ export default {
   menu: {
     locale: true
   },
-  title: 'Ikaros Admin',
+  title: 'Ikaros Web',
   pwa: false,
   iconfontUrl: '',
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'
